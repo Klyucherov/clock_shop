61309c19438b5c18144bd940e8ca2e9fd3336cd5
Klyucherov <73936820+Kluicherov@users.noreply.github.com>
2020-12-26 18:37:04 +0300

    clock_shop
    clock_shop

---
+++ config/params.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'admin_email' => 'evgeniy.kly@mail.ru',
+    'shop_name' => 'Магазин ishop2.loc',
+    'pagination' => 3,
+    'smtp_host' => 'smtp.ukr.net',
+    'smtp_port' => '2525',
+    'smtp_protocol' => 'ssl',
+    'smtp_login' => 'evgeniy.kly@mail.ru',
+    'smtp_password' => '748857119gagazA',
+    'img_width' => 125,
+    'img_height' => 200,
+    'gallery_width' => 700,
+    'gallery_height' => 1000,
+];
